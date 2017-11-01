78d0f2361b9f3614f07044e58e69b85528abe7bf
khanh <khanhnguyen1123@gmail.com>
2017-11-01 14:41:22 -0700

    Added about page

---
+++ src/main/java/com/devopsbuddy/web/controllers/advices/CopyController.java
@@ -0,0 +1,18 @@
+
+
+package com.devopsbuddy.web.controllers.advices;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by tedonema on 13/03/2016.
+ */
+@Controller
+public class CopyController {
+
+    @RequestMapping("/about")
+    public String about() {
+        return "copy/about";
+    }
+}
